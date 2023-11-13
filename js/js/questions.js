cdb5078e89d0c8b76084c9a1f1262881dd7318e5
NF237 <140411689+NF237@users.noreply.github.com>
2023-11-13 10:20:58 -0300

    Update questions.js

--- js/js/questions.js
+++ js/js/questions.js
@@ -1,8 +1,7 @@
-// creating an array and passing the number, questions, options, and
-answers
+// creating an array and passing the number, questions, options, and answers
 let questions = [
-{
-numb: 1,
+  {
+    numb: 1,
 question: "O que matou o willian afton?",
 answer: "A gotera",
 options: [
@@ -44,7 +43,6 @@ options: [
 "8min",
 "7,5min"
 ]
-
 },
 {
 numb: 5,
@@ -55,21 +53,20 @@ options: [
 "2",
 "3",
 "4"
-]
-},
-// you can uncomment the below codes and make duplicate as more as you
-want to add question
-// but remember you need to give the numb value serialize like
-1,2,3,5,6,7,8,9.....
-// {
-// numb: 6,
-// question: "Your Question is Here",
-// answer: "Correct answer of the question is here",
-// options: [
-// "Option 1",
-// "option 2",
-// "option 3",
-// "option 4"
-// ]
-// },
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
 ];
